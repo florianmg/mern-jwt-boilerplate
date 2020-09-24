780e375640dad0c6dc4a39eca92905ed49e98a80
Florian <mesagouiffes@gmail.com>
2020-09-24 14:48:07 +0200

    feat - generate nav-bar component

---
+++ client/src/components/nav-bar/NavBar.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./NavBar.scss";
+
+const NavBar = () => {
+  return (
+    <div>
+      <p>NavBar component</p>
+    </div>
+  );
+};
+
+export default NavBar;
