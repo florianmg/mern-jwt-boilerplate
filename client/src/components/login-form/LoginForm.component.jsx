ed5b7b421157f3c80388d11d2cf8cd7550641f74
Florian <mesagouiffes@gmail.com>
2020-09-24 14:17:09 +0200

    feat - generate login-form + register-form component

---
+++ client/src/components/login-form/LoginForm.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+  import './LoginForm.scss';
+  
+  const LoginForm = () => {
+    return (
+      <div>
+        <p>LoginForm component</p>
+      </div>
+    );
+  };
+  
+  export default LoginForm;
+  
---
+++ client/src/components/register-form/RegisterForm.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+  import './RegisterForm.scss';
+  
+  const RegisterForm = () => {
+    return (
+      <div>
+        <p>RegisterForm component</p>
+      </div>
+    );
+  };
+  
+  export default RegisterForm;
+  
